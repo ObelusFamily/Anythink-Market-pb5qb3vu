44fc35efd7d8d94f36e4b2feb2a5a3defcef07c8
Sergei Ryzhko <ryzhkosergej@gmail.com>
2023-12-16 10:35:23 +0300

    Fix bug

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
 const connection = process.env.MONGODB_URI;
 mongoose.connect(connection);
+require("../models/Item");
+require("../models/User");
+require("../models/Comment");
 
-const User = mongoose.model("User");
-const Item = mongoose.model("Item");
-const Comment = mongoose.model("Comment");
-
-const options = { upsert: true, new: true };
 async function seedDatabase() {
+  const Item = mongoose.model("Item");
+  const Comment = mongoose.model("Comment");
+  const User = mongoose.model("User");
+  const options = { upsert: true, new: true };
   for (let i = 0; i < 100; i++) {
     const user = { username: `user${i}`, email: `user${i}@gmail.com` };
     const options = { upsert: true, new: true };
     const createdUser = await User.findOneAndUpdate(user, {}, options);
-    
+
     const item = {
       slug: `slug${i}`,
       title: `title ${i}`,
@@ -20,7 +22,7 @@ async function seedDatabase() {
       seller: createdUser,
     };
     const createdItem = await Item.findOneAndUpdate(item, {}, options);
-    
+
     if (!createdItem?.comments?.length) {
       let commentIds = [];
       for (let j = 0; j < 100; j++) {
